ce5bad87c19eb219d1cda733438d23421e239a95
Md Arman Ali <www.abraryasir76@gmail.com>
2024-02-14 21:54:09 +0600

    Keyboard function added

--- js/style.js
+++ js/style.js
@@ -1,3 +1,22 @@
-function play(){
+// function play(){
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
     
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden');
+// }
+
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+    console.log('Your random alphabet',alphabet);
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+}
+
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
 }
